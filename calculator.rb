989219cd8f127f6ed46058785bc8feda0a8c869c
unknown <alumnos@D49-16.upc.edu.pe>
2012-09-13 20:44:04 -0500

    Commit Test03

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ module Calculator
 		return 0
 	else
 		arr=input.split(",")
-		return arr[0]
+		return arr[0].to_i + arr[1].to_i
 	end
   end
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -5,7 +5,10 @@ describe Calculator do
   it 'should return 0 on empty string' do
     Calculator.add('').must_equal 0
   end
-  it 'should return 1 =1 on empty string' do
-    Calculator.add('1').must_equal "1"
+  it 'should return 1 on 1' do
+    Calculator.add('1').must_equal 1
+  end
+  it 'should return 3 on 1,2' do
+    Calculator.add('1,2').must_equal 3
   end
 end
